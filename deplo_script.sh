90f26459b0ccafb0533e9157d81d35b3670e64cc
Nishant <makkar.nishant123@gmail.com>
2017-06-16 11:01:33 +0530

    sample

--- deplo_script.sh
+++ deplo_script.sh
@@ -1,4 +1,4 @@
 git add . 
 git commit -am "$1"
-git push origin master
+git push origin master 2
 curl -u admin:0859ca4446544e66973d52951757c9de  http://localhost:8080/job/maven_job_git_2/build?token=CI_test
